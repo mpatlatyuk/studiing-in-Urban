ad7eddcd137f430e28a521ab0eee2f624a4dc83c
mpatlatyuk <mikemp888@gmail.com>
2024-07-22 21:24:20 +1000

    Update module 3_2.py

--- module 3_2.py
+++ module 3_2.py
@@ -3,34 +3,33 @@ def send_email(message, recipient,*, sender='university.help@gmail.com') -> str:
 # 2. проверяет на отправку самому себе
 # 3. проверяет на отправителя по умолчанию"""
 
-    text_1 = f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}'
-    text_2 = f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}'
-    text_3 = f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}'
+    #text_1 = f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}'
+    #text_2 = f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}'
+    #text_3 = f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}'
 
     #return text_2
     #return text_3
+    text = ''
 
-
-    if  '@' not in {sender} or '@' not in {recipient}:
-        print(text_1)
+    if '@' not in {sender} or '@' not in {recipient}:
+        print(text == f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
     elif '.com' not in {sender} or 'com' not in {recipient}:
-        print(text_1)
+        print(text == f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
     elif '.ru' not in {sender} or '.ru' not in {recipient}:
-        print(text_1)
+        print(text == f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
     elif '.net' not in {sender} or '.net' not in {recipient}:
-        print(text_1)
-
+        print(text == f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
 
     elif recipient == sender:
-        print('Нельзя отправить письмо самому себе')
+        print(text == 'Нельзя отправить письмо самому себе')
 
-    sender = True
-    if sender:
-        print(text_2)
+    #sender = True
+    if sender == True:
+        print(text == f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
     else:
-        print(text_3)
+        print(text == f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
 
-    #return text
+    return text
 
 
 send_email('Это сообщение - для проверки связи', 'vasyok1337@gmail.com')
